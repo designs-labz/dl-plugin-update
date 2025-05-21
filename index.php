aba2619ba8e73e9ed9a9b6f0a113a4b58250c402
Bhavanaditya D R <adityabhavan4@gmail.com>
2025-05-21 22:20:59 +0530

    Chang the version

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * Plugin Name:           DL Plugin Update Checker
  * Plugin URI:            https://github.com/designs-labz/dl-plugin-update/
  * Description:           DesignsLabz plugin update checker.
- * Version:               1.0.5
+ * Version:               1.0.6
  * Requires PHP:          7.4
  * Requires at least:     6.1
  * Tested up to:          6.8.2
