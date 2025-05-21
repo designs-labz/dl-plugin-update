56e1125e590000561ab81fa44e39f2194a19683e
Bhavanaditya D R <adityabhavan4@gmail.com>
2025-05-22 01:14:58 +0530

    Chang the version 2

--- dl-plugin-update.php
+++ dl-plugin-update.php
@@ -3,7 +3,7 @@
  * Plugin Name:           DL Plugin Update Checker
  * Plugin URI:            https://github.com/designs-labz/dl-plugin-update/
  * Description:           DesignsLabz plugin update checker.
- * Version:               1.0.12
+ * Version:               1.0.13
  * Requires PHP:          7.4
  * Requires at least:     6.1
  * Tested up to:          6.8.2
@@ -31,5 +31,14 @@ new GitHubUpdater([
 	'github_user'   => 'designs-labz', // GitHub username
 	'github_repo'   => 'dl-plugin-update', // Repo name
 	'plugin_slug'   => plugin_basename(__FILE__),
-	'version'       => '1.0.12',
+	'version'       => '1.0.13', // Current version
+	'author'        => 'DesignsLabz', // Author name
+	'author_uri'    => 'https://designslabz.com/', // Author URI
+	'plugin_uri'    => 'https://github.com/designs-labz/dl-plugin-update/', // Plugin URI
+	'license'       => 'GPL-3.0-or-later', // License
+	'license_uri'   => 'https://www.gnu.org/licenses/gpl-3.0.html', // License URI
+	'text_domain'   => 'designslabz', // Text domain
+	'update_url'    => 'https://api.github.com/repos/designs-labz/dl-plugin-update/releases/latest', // Update URL
+	'update_check'  => true, // Enable update check
+	'update_info'   => true, // Enable update info
 ]);
